1f75db50f8a6b168e48c20d6d25e64eef570d2e2
Mika Nokka <mika.nokka@unikie.com>
2021-12-30 15:16:46 +0200

    added new node. added script to launch nodes from one shell

---
+++ src/publisher2.cpp
@@ -0,0 +1,45 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "publisher.h"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+ * member function as a callback from the timer. */
+
+MinimalPublisher::MinimalPublisher()
+: Node("minimal_publisher2"), count_(0)
+{
+  publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+  timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
+}
+
+void MinimalPublisher::timer_callback(void)
+{
+  auto message = std_msgs::msg::String();
+  message.data = "Messaging messaging " + std::to_string(count_++);
+  RCLCPP_INFO(this->get_logger(), "Publishing message: '%s'", message.data.c_str());
+  publisher_->publish(message);
+}
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ startnodes.sh
@@ -0,0 +1,2 @@
+gnome-terminal --tab --title="mytalker" --command="bash -c 'source /opt/ros/foxy/setup.bash; . install/local_setup.bash;  ros2 run multinodes mytalker ; $SHELL'" --tab --title="mylistener" --command="bash -c 'source /opt/ros/foxy/setup.bash; . install/local_setup.bash;  ros2 run multinodes mylistener ; $SHELL'"
+ 
