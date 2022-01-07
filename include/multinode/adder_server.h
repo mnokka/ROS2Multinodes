bf5bd507b85e0814ea98e498163682e56bc71d8d
Mika Nokka <mika.nokka@unikie.com>
2022-01-07 13:30:55 +0200

    DP-1531 added basic server

---
+++ include/multinode/adder_server.h
@@ -0,0 +1,18 @@
+/*
+ * adder_server.h
+ *
+ *  Created on: Jan 7, 2022
+ *      Author: mika
+ */
+
+#ifndef INCLUDE_MULTINODE_ADDER_SERVER_H_
+#define INCLUDE_MULTINODE_ADDER_SERVER_H_
+
+
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
+
+#include <memory>
+
+
+#endif /* INCLUDE_MULTINODE_ADDER_SERVER_H_ */
